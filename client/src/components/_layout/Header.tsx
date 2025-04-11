faca2dcdcc56b1f7668c8036b1053516f08f4aa8
crossovka <best-login-mail@yandex.ru>
2025-04-11 20:02:52 +0900

    add links in header

--- client/src/components/_layout/Header.tsx
+++ client/src/components/_layout/Header.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
 import { ThemeToggle } from '@/components/elements/ThemeToggler';
 import { useAppSelector } from '@/redux/store';
 import { selectUserName } from '@/redux/slices/auth/auth.selectors';
@@ -13,7 +15,7 @@ export const Header = () => {
 		setIsClient(true);
 	}, []);
 
-	if (!isClient) return null; // пока SSR → не рендерим, чтобы не было различий hidration error
+	if (!isClient) return null;
 
 	return (
 		<header
@@ -25,9 +27,28 @@ export const Header = () => {
 				backgroundColor: '#f0f0f0',
 			}}
 		>
-			<h1 style={{ margin: 0 }}>
-				{userName && <span>{`Добро пожаловать, ${userName}`}</span>}
-			</h1>
+			<div>{userName && <span>{`Добро пожаловать, ${userName}`}</span>}</div>
+			<nav>
+				<ul
+					style={{
+						display: 'flex',
+						listStyle: 'none',
+						gap: '1rem',
+						margin: 0,
+						padding: 0,
+					}}
+				>
+					<li>
+						<Link href="/categories">Категории</Link>
+					</li>
+					<li>
+						<Link href="/movies">Фильмы</Link>
+					</li>
+					<li>
+						<Link href="/user/favorites">Избранное</Link>
+					</li>
+				</ul>
+			</nav>
 			<ThemeToggle />
 		</header>
 	);
